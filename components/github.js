1b43cf7a5ce20ac7159b8cc7f19e6e0df839d823
Jesse Bryan <jesse@winneon.moe>
2017-09-02 05:21:37 -0500

    testing: fixed the style guide

--- components/github.js
+++ components/github.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 class GitHub extends React.Component {
   render() {
-    return <a href={ `https://github.com/${this.props.append}` } target='_blank'>{ this.props.children }</a>
+    return <a href={ `https://github.com/${this.props.append}` } target="_blank">{ this.props.children }</a>
   }
+}
+
+GitHub.propTypes = {
+  append: PropTypes.string,
+  children: PropTypes.element.isRequired
+}
+
+GitHub.defaultProps = {
+  append: ''
 }
 
 export default GitHub
--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,15 @@ import Sass from '../components/sass'
 import GitHub from '../components/github'
 
 export default () => (
-  <div className='content'>
+  <div className="content">
     <Head>
       <title>seekrit</title>
     </Head>
     <div>
-      <Button color='primary'>Upload</Button>
+      <Button color="primary">Upload</Button>
     </div>
     <footer>
-      <GitHub append='winneon/seekrit'>seekrit</GitHub>, by <GitHub append='winneon'>winneon</GitHub>
+      <GitHub append="winneon/seekrit">seekrit</GitHub>, by <GitHub append="winneon">winneon</GitHub>
     </footer>
     <Sass />
   </div>
